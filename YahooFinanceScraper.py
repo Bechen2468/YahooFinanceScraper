c1514f62691b881681e6f7dd4cb9d48b1ac0c745
Bechen2468 <leon4.2@outlook.com>
2024-03-12 03:51:57 +0100

    YFScraper.sleep() implemented. YFScraper added runtime_Data attribute.

--- YahooFinanceScraper.py
+++ YahooFinanceScraper.py
@@ -1,8 +1,22 @@
 import Runtime_Data
+import random
+import time
 
 class YFScraper:
     def __init__(self):
+        self.runtime_Data = Runtime_Data()
         return
     
+    def sleep(self, minSeconds, maxSeconds):
+        sleep_time = random.uniform(minSeconds, maxSeconds)
+        self.runtime_Data.sleep_duration += sleep_time
+        time.sleep(sleep_time)
+        return
+
+    
     def scrape(self):
+        self.runtime_Data.start()
+
+
+        self.runtime_Data.end()
         return
